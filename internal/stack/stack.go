62b27e69fe703635ed56bdcdd2fcfa14a0684054
sstanton <steven@thesimpledev.com>
2025-11-09 12:38:37 -0500

    test: moved to test more behaviors instead of implementation details

--- internal/stack/stack.go
+++ internal/stack/stack.go
@@ -5,7 +5,7 @@ import "sync"
 
 type Stack struct {
 	items []int
-	mu    sync.Mutex
+	mu    sync.RWMutex
 }
 
 func New() *Stack {
@@ -16,18 +16,36 @@ func New() *Stack {
 
 func (s *Stack) Push(v int) {
 	s.mu.Lock()
+	defer s.mu.Unlock()
 	s.items = append(s.items, v)
-	s.mu.Unlock()
 }
 
 func (s *Stack) Pop() (int, bool) {
 	s.mu.Lock()
+	defer s.mu.Unlock()
 	if len(s.items) == 0 {
 		return 0, false
 	}
 	pos := len(s.items) - 1
 	v := s.items[pos]
 	s.items = s.items[:pos]
-	s.mu.Unlock()
 	return v, true
+}
+
+func (s *Stack) Peek() (int, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	if len(s.items) == 0 {
+		return 0, false
+	}
+
+	pos := len(s.items) - 1
+
+	return s.items[pos], true
+}
+
+func (s *Stack) Len() int {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return len(s.items)
 }
